9a0a7ad636efec4c6da848ee80b96689b5751ff9
Sara Qin <saraqin@Saras-MacBook-Air.local>
2023-08-07 17:25:17 -0400

    Create new library project

---
+++ Mini_Projects/Library/src/main/java/sara/library/Library.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sara.library;
+
+/**
+ *
+ * @author saraqin
+ */
+public class Library {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
