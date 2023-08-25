865ff72b663afa13ac81779220e4c3fc12d91ded
Sara Qin <sara940223@gmail.com>
2023-08-24 21:04:13 -0400

    Circular array

--- Mini_Projects/ListApp/src/main/java/sara/listapp/List.java
+++ Mini_Projects/ListApp/src/main/java/sara/listapp/List.java
@@ -4,139 +4,114 @@ package sara.listapp;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author saraqin
  */
 public class List {
-    
+
     int length;
     int nextSpace = 0;
-    String[] arr = new String [length];
-   
-    public List(int length)
-    {
+    String[] arr = new String[length];
+
+    public List(int length) {
         this.length = length;
     }
-   
+
     public int increaseLength()//increase 5
     {
         length = length + 5;
         return length;
     }
-    
-    public int getNextSpace()
-    {
+
+    public int getNextSpace() {
         return nextSpace;
     }
-    
-    public String getElement(int n)
-    {
+
+    public String getElement(int n) {
         return arr[n];
     }
-    
-    public void renewList()
-    {
-        if (nextSpace>=arr.length)
-        {
+
+    public void renewList() {
+        if (nextSpace >= arr.length) {
             length = increaseLength();
-            String[] arrTemperary = new String [length];
-            for (int i = 0; i<arr.length; i++)
-            {
-                arrTemperary[i]=arr[i];
+            String[] arrTemperary = new String[length];
+            for (int i = 0; i < arr.length; i++) {
+                arrTemperary[i] = arr[i];
             }
-            arr=arrTemperary; 
+            arr = arrTemperary;
         }
     }
-    
-    public void add(String string)
-    {
+
+    public void add(String string) {
         renewList();
-        
-        arr[nextSpace]=string;
+
+        arr[nextSpace] = string;
         nextSpace++;
     }
-    
-    public void add(int index, String string)
-    {
-           renewList();
-           String temp;
-           for (int i=index;i<arr.length;i++)
-           {
-                temp = arr[i];
-                arr[i]=string;
-                string = temp;
-           }
-           nextSpace++;
-    }
-    
-    public void remove(int index)
-    {
-        if(index==arr.length-1)
-        {
-            arr[index]=null;
+
+    public void add(int index, String string) {
+        renewList();
+        String temp;
+        for (int i = index; i < arr.length; i++) {
+            temp = arr[i];
+            arr[i] = string;
+            string = temp;
         }
-        else
-        {
-            if(index<arr.length-1)
-            {
-         
-                for (int i=index+1; i<arr.length;i++)
-                {
-                    arr[index]=arr[i];
+        nextSpace++;
+    }
+
+    public void remove(int index) {
+        if (index == arr.length - 1) {
+            arr[index] = null;
+        } else {
+            if (index < arr.length - 1) {
+
+                for (int i = index + 1; i < arr.length; i++) {
+                    arr[index] = arr[i];
                     index++;
                 }
-                arr[arr.length-1] = null;
+                arr[arr.length - 1] = null;
             }
         }
     }
-    
-    public void remove(String string)
-    {
-        int index=-1;
-                
-        for (int i=0; i<arr.length;i++)//find the element
+
+    public void remove(String string) {
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++)//find the element
         {
-            if(string.equals(arr[i]))
-            {
-                arr[i]=null;
+            if (string.equals(arr[i])) {
+                arr[i] = null;
                 index = i;
                 break;
             }
         }
-        
-        if(index>=0)
-        {
-        remove(index);
+
+        if (index >= 0) {
+            remove(index);
         }
     }
-    
-    public String toString()
-    {
-        for (int i=0; i<arr.length; i++)
-        {
-            if(arr[i]!=null)
-            {
-            System.out.print(arr[i]+" ");
+
+    public String toString() {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                System.out.print(arr[i] + " ");
             }
-            
+
         }
         return "";
     }
-    
-    public int size ()
-    {
+
+    public int size() {
         int size = 0;
-        for (int i=0; i<arr.length;i++)
-        {
-            if(arr[i]!=null)
-            {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
                 size++;
             }
-            
+
         }
         return size;
     }
-    
+
 }
--- Mini_Projects/ListApp/src/main/java/sara/listapp/ListApp.java
+++ Mini_Projects/ListApp/src/main/java/sara/listapp/ListApp.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package sara.listapp;
 
 /**
@@ -10,17 +9,15 @@ package sara.listapp;
  */
 public class ListApp {
 
-    
-    
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+
         // Initial size of list is 5
         // List linearly by 5 when reaching maximum capacity.
-        List list = new List(5); 
-        
+        List list = new List(5);
+
         list.add("A");
-        list.add("B");        
+        list.add("B");
         list.add("C");
         list.add("D");
         list.add("D"); // This is duplicate.  It should still add to list.
@@ -29,29 +26,27 @@ public class ListApp {
         list.add("G");
         list.add("H");
         list.add("I");
-        
-        
+
         list.remove(6); // Remove element at index 6.
         System.out.println(list.toString()); // A B C D D E G H I
-        
+
         list.remove("E");
         System.out.println(list.toString()); // A B C D D G H I
-        
-        list.add(6, "E"); 
+
+        list.add(6, "E");
         list.add("E");
-        
+
         System.out.println(list.toString()); // A B C D D G E H I E
-        
+
         list.add("A");
         list.add("B");
-        
+
         list.remove("A");
         list.remove("B");
-        
+
         System.out.println(list.toString()); // C D D G E H I E A B
-        
+
         System.out.println(list.size()); // 10
     }
-    
-    
+
 }
