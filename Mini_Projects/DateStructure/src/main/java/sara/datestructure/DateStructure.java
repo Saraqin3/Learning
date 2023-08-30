3b6c802346d0958792449ad323cf31e12e791fff
Sara Qin <sara940223@gmail.com>
2023-08-30 00:05:01 -0400

    Created Data Structure Learning Project

---
+++ Mini_Projects/DateStructure/src/main/java/sara/datestructure/DateStructure.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sara.datestructure;
+
+/**
+ *
+ * @author saraqin
+ */
+public class DateStructure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Mini_Projects/DateStructure/src/main/java/sara/datestructure/Node.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sara.datestructure;
+
+/**
+ *
+ * @author saraqin
+ */
+public class Node {
+    
+    int data;
+    Node nextNode;
+    
+    public Node (int data, Node nextNode)
+    {
+        this.data=data;
+        this.nextNode=nextNode;
+    }
+    
+    public int getData ()
+    {
+        return data;
+    }
+    
+    public Node getNextNode ()
+    {
+        return nextNode;
+    }
+    
+    public void setData (int data)
+    {
+        this.data=data;
+    }
+    
+    public void setNextNode
+    
+}
