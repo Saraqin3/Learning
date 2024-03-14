9ce0098999b306a29af1e0b4a40733dfa99ce593
Sara Qin <sara940223@gmail.com>
2024-03-13 23:59:02 -0400

    Added JS

--- Learning_HTML/Basic/Twitter/js/script.js
+++ Learning_HTML/Basic/Twitter/js/script.js
@@ -21,10 +21,38 @@ document.getElementById("inputFile").addEventListener("change", function(){
 
 
  document.getElementById("postButton").addEventListener("click", function(){
-    if (this.file[0]) {
+    
+    if (document.getElementById("inputFile").files[0]||document.getElementById("writeSpace").value!="") {
+        document.getElementById("p2").innerHTML = '
 
+        <div class="profilePicBox" id="element0">
+                    <img class="profilePic" id="profilePic" src="Default_pfp.svg.png" alt="Profile Image">
+                </div>
+                <div class="editArea" id="editArea">
+                    <div>
+                        <input type="text" class="writeSpace" id="writeSpace" placeholder="What's new?" maxlength="50">
+                        <div>
+                            <img class="picForReview" id="picForReview" src="" alt="Pic For Review">
+                        </div>
+                    </div>
+                    <div class="line" id="line"></div>
+                    <div class="uploadBar" id="uploadBar">
+                        <div class="picForUploadBox" id="picForUploadBox">
+                            <input type="file" id="inputFile" style="display: none;">
+                            <img class="picForUpload" id="picForUpload" src="images.png" alt="ImagesForUpload" onclick="document.getElementById('inputFile').click();">
+                        </div>
+                        <div class="middleBlankSpace" id="middleBlankSpace"></div>
+                        <div class="postButtonBox" id="postButtonBox">
+                            <button class="postButton" id="postButton">Post</button>
+                        </div>
+
+                    </div>
+                    
+                    
+                </div>'
+        + document.getElementById("p2").innerHTML;
     }
     else {
-        
+
     }
  })
